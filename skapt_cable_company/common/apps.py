641f9d9fe8d7c4d80983d77041d4064272f25d0b
kiruthihan nagarajan <kiruthihan10@gmail.com>
2024-07-05 15:35:35 +0530

    docstring update

--- skapt_cable_company/common/apps.py
+++ skapt_cable_company/common/apps.py
@@ -1,6 +1,12 @@
+"""App Configuration for Common Models App"""
+
 from django.apps import AppConfig
 
 
 class CommonConfig(AppConfig):
+    """
+    A Class to Do Common Models App Configurations
+    """
+
     name = "common"
     verbose_name = "Common Models"
