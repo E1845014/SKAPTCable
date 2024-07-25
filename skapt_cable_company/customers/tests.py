0a019f3ab7b05dac7391bd1198a2ffbd017220ab
kiruthihan nagarajan <kiruthihan10@gmail.com>
2024-07-25 19:40:02 +0530

    Customers Test

--- skapt_cable_company/customers/tests.py
+++ skapt_cable_company/customers/tests.py
@@ -1,12 +1,62 @@
+"""
+Module to contain all Customers App View Controller Codes
+"""
+
+# pylint: disable=imported-auth-user
+
+from django.contrib.auth.models import User
 from django.forms import Form
 from datetime import date
 
 from common.tests import BaseTestCase
 
-# Create your tests here.
+
+class CustomerBaseTestCase(BaseTestCase):
+    """
+    Bese Tast Functionalities for Customer App Testing
+    """
 
 
-class AddCustomerTestCase(BaseTestCase):
+class CustomersTestCase(CustomerBaseTestCase):
+    """
+    Test Cases for testing Customers List Page and their functionalities
+    """
+
+    def test_page_renders_for_employees(self):
+        """
+        Test if the customers page renders for employee user
+        """
+        employee = self.generate_employees(1)[0]
+        self.login_as_employee(employee)
+        response = self.client.get("/customers/")
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed("customers.html")
+        self.client.logout()
+        response = self.client.get("/customers/")
+        self.assertNotEqual(response.status_code, 200)
+
+    def test_page_not_renders_for_non_employees(self):
+        """
+        Test if the customers page not renders for non-employees
+        """
+        user = User.objects.create_user(
+            "username", "email@email.email", self.raw_password
+        )
+        self.client.login(username=user.username, password=self.raw_password)
+        response = self.client.get("/customers/")
+        self.assertEqual(response.status_code, 403)
+
+    def test_shows_all_employees(self):
+        """
+        Test if all the employees are shown
+        """
+        customers = self.generate_customers()
+        self.login_as_employee(customers[0].area.agent)
+        response = self.client.get("/customers/")
+        self.assertEqual(len(response.context["customers"]), len(customers))
+
+
+class AddCustomerTestCase(CustomerBaseTestCase):
 
     def setUp(self):
         """
@@ -37,7 +87,7 @@ class AddCustomerTestCase(BaseTestCase):
         request_object = {}
         new_employee_phone_number = self.get_random_phone_number()
         user_form: Form = get_response.context["user_form"]
-        customer_form : Form = get_response.context["customer_form"]
+        customer_form: Form = get_response.context["customer_form"]
         area_choices = customer_form.fields["area"].choices
         for field in user_form.fields:
             if field == "email":
