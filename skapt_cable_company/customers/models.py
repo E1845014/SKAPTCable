cc586d5924d96a8f057c721773c02b55b89b1bf5
kiruthihan nagarajan <kiruthihan10@gmail.com>
2024-08-04 21:50:52 +0530

    Module customers.models

--- skapt_cable_company/customers/models.py
+++ skapt_cable_company/customers/models.py
@@ -6,6 +6,9 @@ from common.models import Customer
 
 
 def generate_customer_number(customer: Customer):
+    """
+    Generate Customer Number based on the area
+    """
     area = customer.area
     number_of_customers_in_the_area = Customer.objects.filter(area=area).count()
     return f"{area.name[:3]}{number_of_customers_in_the_area + 1}"
