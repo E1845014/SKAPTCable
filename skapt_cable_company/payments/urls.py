00b1f60395ed0238595caf503d882a35f83b27d0
kiruthihan nagarajan <kiruthihan10@gmail.com>
2024-11-09 12:11:11 +0530

    View All Payments

--- skapt_cable_company/payments/urls.py
+++ skapt_cable_company/payments/urls.py
@@ -7,6 +7,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path("allPayments", views.get_all_payments, name="all_payments"),
     path("<str:username>/addPayment", views.add_customer_payment, name="add_payment"),
     path("<str:username>/payments", views.get_customer_payments, name="view_payments"),
 ]
--- skapt_cable_company/payments/views.py
+++ skapt_cable_company/payments/views.py
@@ -7,12 +7,44 @@ from django.http import HttpResponse, HttpRequest
 from django.template import loader
 from django.shortcuts import redirect, get_object_or_404
 from django.core.exceptions import BadRequest, PermissionDenied
+from django.core.paginator import Paginator
 
 from common.models import Customer, Payment, Employee
 
 from .forms import PaymentForm
 
 # Create your views here.
+
+
+@login_required
+def get_all_payments(request: HttpRequest):
+    """
+    Get all Payments
+    """
+    if not Employee.objects.filter(user=request.user).exists():
+        raise PermissionDenied
+    template = loader.get_template("all_payments.html")
+    size = request.GET.get("size", "10")
+    if size.isnumeric():
+        size = int(size)
+    else:
+        size = 10
+    page_number = request.GET.get("page", "1")
+    if page_number.isnumeric():
+        page_number = int(page_number)
+    else:
+        page_number = 1
+    payments = Payment.objects.all()
+    paginator = Paginator(payments, size)
+    return HttpResponse(
+        template.render(
+            {
+                "paginator": paginator,
+                "payments": paginator.page(page_number),
+            },
+            request,
+        )
+    )
 
 
 @login_required
