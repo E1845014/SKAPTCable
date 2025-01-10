fc7a51ca633270c2b89dc03237bc661a7d947af2
kiruthihan nagarajan <kiruthihan10@gmail.com>
2025-01-10 09:21:56 +0530

    Add URL pattern for password update functionality

--- skapt_cable_company/login/urls.py
+++ skapt_cable_company/login/urls.py
@@ -11,4 +11,5 @@ urlpatterns = [
     path("accounts/login/", views.index, name="index"),
     path("home", views.home, name="home"),
     path("logout", views.logout_user, name="logout"),
+    path("password", views.update_password, name="update_password"),
 ]
