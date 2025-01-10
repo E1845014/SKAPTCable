6e13355145cb304638dead66cda9f74ddab32a00
kiruthihan nagarajan <kiruthihan10@gmail.com>
2025-01-10 09:21:44 +0530

    Add EmployeePasswordChangeForm for enhanced employee password management

--- skapt_cable_company/login/forms.py
+++ skapt_cable_company/login/forms.py
@@ -2,7 +2,10 @@
 Module for all Login App Related Forms
 """
 
+from typing import Any
 from django import forms
+from django.contrib.auth.base_user import AbstractBaseUser
+from django.contrib.auth.forms import PasswordChangeForm
 
 
 class LoginForm(forms.Form):
@@ -14,6 +17,17 @@ class LoginForm(forms.Form):
     password = forms.CharField(widget=forms.PasswordInput)
 
     def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field in self.fields.values():
+            field.widget.attrs["class"] = "input is-rounded"
+
+
+class EmployeePasswordChangeForm(PasswordChangeForm):
+    """
+    Password Change form for Employees
+    """
+
+    def __init__(self, *args: Any, **kwargs: Any) -> None:
         super().__init__(*args, **kwargs)
         for field in self.fields.values():
             field.widget.attrs["class"] = "input is-rounded"
